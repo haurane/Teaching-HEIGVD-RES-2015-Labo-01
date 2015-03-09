d0de69052ab95de3fcd988f9ec29aada816bafbb
haurane <haurane@gmail.com>
2015-03-09 14:59:45 +0100

    Implementation Uppercase, get next line, NoOp transformer

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -8,19 +8,33 @@ import java.util.logging.Logger;
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+	private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	/**
+	 * This method looks for the next new line separators (\r, \n, \r\n) to
+	 * extract the next line in the string passed in arguments.
+	 * 
+	 * @param lines
+	 *            a string that may contain 0, 1 or more lines
+	 * @return an array with 2 elements; the first element is the next line with
+	 *         the line separator, the second element is the remaining text. If
+	 *         the argument does not contain any line separator, then the first
+	 *         element is an empty string.
+	 */
+	public static String[] getNextLine(String lines) {
+		String[] lineArray = new String[2];
+		int i = -1;
+		if ((i = lines.indexOf("\r\n")) != -1)
+			i++;
+		else if ((i = lines.indexOf('\r')) == -1)
+			if ((i = lines.indexOf('\n')) == -1) {
+				lineArray[0] = new String();
+				lineArray[1] = lines;
+				return lineArray;
+			}
+		lineArray[0] = lines.substring(0, i + 1);
+		lineArray[1] = lines.substring(i + 1);
+		return lineArray;
+	}
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,23 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	  String output = str.toUpperCase();
+	  super.write(output, off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    char[] output = new char[cbuf.length];
+    for(int i = 0; i<cbuf.length; i++){
+    	output[i] = Character.toUpperCase(cbuf[i]);
+    }
+    super.write(output, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    int output = Character.toUpperCase(c);
+    super.write(output);
   }
 
 }
