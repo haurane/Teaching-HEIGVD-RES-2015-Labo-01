05a9344e093106ed9cb1d503e666bae7b8070a5d
haurane <haurane@gmail.com>
2015-03-10 09:37:13 +0100

    Visitor & Explorer

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -7,16 +7,27 @@ import java.io.File;
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
+ * node (file and directory). When the explorer reaches a directory, it visits
+ * all files in the directory and then moves into the subdirectories.
  * 
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void explore(File rootDirectory, IFileVisitor vistor) {
+		vistor.visit(rootDirectory);
+		if (rootDirectory.isDirectory()) {
+			for (File f : rootDirectory.listFiles()) {
+				if (f.isFile())
+					vistor.visit(f);
+			}
+			for (File f : rootDirectory.listFiles()) {
+				if (f.isDirectory()) {
+					explore(f, vistor);
+				}
+			}
+		}
+	}
 
 }
